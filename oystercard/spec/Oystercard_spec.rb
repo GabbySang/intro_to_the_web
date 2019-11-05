1d164a6842aed08858df9ff0caacc987fb949494
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-05 17:15:46 +0000

    start of part 6, maximum

--- oystercard/spec/Oystercard_spec.rb
+++ oystercard/spec/Oystercard_spec.rb
@@ -16,4 +16,10 @@ it "should deposit money put in by the user" do
   oystercard.top_up(10)
   expect(oystercard.balance).to eq 10
 end
+
+it "raises an error when balance exceeds £90" do
+    oystercard = Oystercard.new
+    expect {oystercard.balance > 90}.to raise_error("Maximum Exceeded")
+    #{#maximum} needs to be put in "Maximum Exceeded"
+end
 end
