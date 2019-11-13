edcb4aed6b175d46b350763e19c52023bc114179
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-13 16:58:28 +0000

    cat with border

---
+++ sinatra/app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+
+get '/' do
+  "hello!"
+end
+
+get '/secret' do
+  "here is a NEW secret message"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
